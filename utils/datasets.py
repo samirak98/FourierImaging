5e1c350fb09560a93e73fe885313fe4d4696eed3
Tim Roith <tim.roith@fau.de>
2023-01-09 16:30:46 +0100

    :hedgehog: add classification F-MNIST

--- utils/datasets.py
+++ utils/datasets.py
@@ -6,7 +6,7 @@ import os
 
 
 #%% 
-def load(conf, test_size=1):
+def load(conf):
     train, valid, test, train_loader, valid_loader, test_loader = [None] * 6
     if conf['name'] == "MNIST":
         conf['im_shape'] = [1,28,28]
@@ -19,18 +19,44 @@ def load(conf, test_size=1):
         transform = transforms.Compose([transforms.ToTensor()])
         train = datasets.MNIST(conf['path'], train=True, download=conf['download'], transform=transform)
         test = datasets.MNIST(conf['path'], train=False, download=conf['download'], transform=transform)
+    elif conf['name'] == "FashionMNIST":
+        conf['im_shape'] = [1,28,28]
+        
+        # set mean and std for this dataset
+        conf['mean'] = 0.5
+        conf['std'] = 0.5
+
+        # load MNIST
+        transform = transforms.Compose([transforms.ToTensor()])
+        train = datasets.FashionMNIST(conf['path'], train=True, download=conf['download'], transform=transform)
+        test = datasets.FashionMNIST(conf['path'], train=False, download=conf['download'], transform=transform)
+    elif conf['name'] == "CIFAR10":
+        conf['im_shape'] = [3,32,32]
+        
+        # set mean and std for this dataset
+        conf['mean'] = torch.tensor([0.4914, 0.4822, 0.4465]).view(-1,1,1)
+        conf['std'] = torch.tensor([0.2023, 0.1994, 0.2010]).view(-1,1,1)
+
+        # load MNIST
+        transform_train = transforms.Compose([transforms.RandomCrop(32, padding=4),
+                                          transforms.RandomHorizontalFlip(),
+                                          transforms.ToTensor()])
+        transform_test = transforms.Compose([transforms.ToTensor()])
+        train = datasets.CIFAR10(conf['path'], train=True, download=conf['download'], transform=transform_train)
+        test = datasets.CIFAR10(conf['path'], train=False, download=conf['download'], transform=transform_test)
     else:
-        raise ValueError("Unknown dataset:" + conf.data_set)
+        raise ValueError("Unknown dataset: " + conf['name'])
    
-    tr_loader, v_loader, te_loader = split_loader(train, test, conf['batch_size'],\
+    tr_loader, v_loader, te_loader = split_loader(train, test,\
+                                                  batch_size = conf['batch_size'],\
+                                                  batch_size_test = conf['batch_size_test'],\
                                                   train_split=conf['train_split'],\
-                                                  test_size=test_size,\
                                                   num_workers=conf['num_workers'])
         
     return tr_loader, v_loader, te_loader
 #%% Define DataLoaders and split in train, valid, test       
-def split_loader(train, test, batch_size, train_split=0.9, test_size=1,\
-                 num_workers=1, seed=42):
+def split_loader(train, test, batch_size=128, batch_size_test=100,\
+                 train_split=0.9, num_workers=1, seed=42):
     total_count = len(train)
     train_count = int(train_split * total_count)
     val_count = total_count - train_count
@@ -38,14 +64,10 @@ def split_loader(train, test, batch_size, train_split=0.9, test_size=1,\
     train, val = torch.utils.data.random_split(train,\
                                 [train_count, val_count],generator=generator)
 
-    if test_size != 1:
-        test_count = int(len(test) * test_size)
-        _count = len(test) - test_count
-        test, _ = torch.utils.data.random_split(test, [test_count, _count])
 
     loader_kwargs = {'shuffle':True, 'pin_memory':True, 'num_workers':num_workers}
     train_loader = DataLoader(train, batch_size=batch_size, **loader_kwargs)
-    valid_loader = DataLoader(val, batch_size=1000, **loader_kwargs)
-    test_loader = DataLoader(test, batch_size=batch_size, **loader_kwargs)
+    valid_loader = DataLoader(val, batch_size=batch_size, **loader_kwargs)
+    test_loader = DataLoader(test, batch_size=batch_size_test, **loader_kwargs)
 
     return train_loader, valid_loader, test_loader
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 from modules.perceptron import perceptron
+from modules.simple_cnn import simple_cnn
+from modules.resnet import load_resnet
 import random
 import numpy as np
 #%% set a fixed seed
@@ -16,15 +18,29 @@ def fix_seed(seed=0):
 #%% load model by type
 def load_model(conf):
     model_conf = conf['model']
+    
     # load activation function
-    if model_conf['activation_function'] == 'ReLU':
-        act_fun = nn.ReLU
+    if 'activation_function' in model_conf:
+        if model_conf['activation_function'] == 'ReLU':
+            act_fun = nn.ReLU
+        else:
+            raise Warning('Unknown activation function: ' + model_conf['activation_function'] +\
+                            ' specified, using ReLU instead.')
+            act_fun = nn.ReLU
+                
     
     if model_conf['type'] == 'perceptron':
         model = perceptron(model_conf['sizes'], act_fun = act_fun,\
                            mean=conf['dataset']['mean'], std=conf['dataset']['std'])
+    elif model_conf['type'] == 'simple_cnn':
+        model = simple_cnn(act_fun = act_fun,\
+                           mean=conf['dataset']['mean'], std=conf['dataset']['std'])
+    elif model_conf['type'] == 'resnet':
+        
+        model = load_resnet(size=model_conf['size'],\
+                            mean=conf['dataset']['mean'], std=conf['dataset']['std'])
     else:
-        raise ValueError('Unknown model type: ' + conf['type'])
+        raise ValueError('Unknown model type: ' + model_conf['type'])
     
     return model
 
@@ -32,6 +48,8 @@ def load_model(conf):
 def init_opt(model, conf):
     if conf['name'] == 'SGD':
         opt = torch.optim.SGD(model.parameters(), lr = conf['lr'], momentum = conf['momentum'])
+    elif conf['name'] == 'Adam':
+        opt = torch.optim.Adam(model.parameters())
     else:
         raise ValueError('Unknown optimizer: ' + conf['name'])
     return opt
