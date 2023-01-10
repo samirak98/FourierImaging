39305176cc31483575a7837c70fd5cad5166ab95
Tim Roith <tim.roith@fau.de>
2023-01-10 12:57:07 +0100

    :hedgehog: interpolation test

--- experiments/tests/MNIST-Res.py
+++ experiments/tests/MNIST-Res.py
@@ -1,19 +1,26 @@
 import torch
+from torchvision import transforms
 import yaml
+import numpy as np
 
-import os, sys
-import os.path
-sys.path.append('../utils/')
 # custom imports
-from utils import helper
-from utils import datasets as data
-import train
+#%% custom imports
+import sys, os
+sys.path.append(os.path.abspath('../../'))
+
+import fourierimaging as fi
+
+from fourierimaging.utils.helpers import load_model, init_opt, fix_seed
+from fourierimaging.modules import TrigonometricResize_2d
+from fourierimaging.utils import datasets as data
+import fourierimaging.train as train
 
 #%% Set up variable and data for an example
-experiment_file = 'utils/experiments/classification/MNIST.yaml'
+experiment_file = '../classification/MNIST.yaml'
 with open(experiment_file) as exp_file:
     conf = yaml.safe_load(exp_file)
 
+conf['dataset']['path'] = '../../../datasets'
 #%% fix random seed
 fix_seed(conf['seed'])
 
@@ -27,5 +34,68 @@ else:
     device = "cpu"
 conf['train']['device'] = device
 model = load_model(conf).to(device)
-path = '../saved_model/perceptron-MNIST'
-model.load_state_dict(torch.load(path))
+path = '../saved_models/perceptron-MNIST'
+model.load_state_dict(torch.load(path, map_location=device))
+
+#%% eval
+downsampling = ['BILINEAR']
+upsampling = ['TRIGO', 'BILINEAR', 'NEAREST', 'BICUBIC']
+combinations = [(u,d) for u in upsampling for d in downsampling]
+
+def select_sampling(name, size):
+    if name == 'BILINEAR':
+        return transforms.Resize(size, interpolation=transforms.InterpolationMode.BILINEAR)
+    elif name == 'NEAREST':
+        return transforms.Resize(size, interpolation=transforms.InterpolationMode.NEAREST)
+    elif name == 'BICUBIC':
+        return transforms.Resize(size, interpolation=transforms.InterpolationMode.BICUBIC)
+    elif name == 'TRIGO':
+        return TrigonometricResize_2d(size)
+    else:
+        raise ValueError('Unknown resize method: ' + name)
+        
+        
+
+size_step = 8
+sizes = np.arange(2,28,size_step)
+orig_size = [28,28]
+
+
+
+def main():
+    model.eval()
+    
+    for m, d in combinations:
+        print(20*'<>')
+        print('Starting test for model sizing: ' + m + ' and data sizing: ' + d)
+        print(20*'<>')
+        acc = 0
+        tot_steps = 0
+        
+        for s in sizes:
+            resize_data = select_sampling(d, [s,s])
+            resize_model = select_sampling(m, orig_size)
+            
+            with torch.no_grad():
+                for batch_idx, (x, y) in enumerate(test_loader):
+                    # get batch data
+                    x, y = x.to(device), y.to(device)
+                    
+                    #resize input
+                    
+                    x = resize_data(x)
+                   
+                    
+                    # evaluate
+                    x = resize_model(x)
+                    pred = model(x)
+                    acc += (pred.max(1)[1] == y).sum().item()
+                    
+                    tot_steps += y.shape[0]
+            print(20*'<>')
+            print('Done for s='+str(s))
+            print('Test Accuracy:', acc/tot_steps)
+            print(20*'<>')
+    
+if __name__ == '__main__':
+    main()
