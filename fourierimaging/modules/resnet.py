bd623ac5e487e986994bab8c6259381c5e68f0cc
Samira Kabri <samira.kabri@fau.de>
2023-01-19 23:23:45 +0100

    :cake: Started to implement extension for even sized spectral kernels
    Co-authored-by: Tim Roith <TimRoith@users.noreply.github.com>

--- fourierimaging/modules/resnet.py
+++ fourierimaging/modules/resnet.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 
-from torchvision.models._api import register_model, Weights, WeightsEnum
+from torchvision.models._api import Weights, WeightsEnum
 
 def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1, padding_mode='zeros') -> nn.Conv2d:
     """3x3 convolution with padding"""
